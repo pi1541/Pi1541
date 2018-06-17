34e53d7ea7ea22ab326c4e8c2bad9aa115043867
Stephen White <pi1541contact@gmail.com>
2018-06-17 19:05:14 +1000

    Incremented build number.

--- src/main.cpp
+++ src/main.cpp
@@ -45,7 +45,7 @@ extern "C"
 #include "sample.h"
 
 unsigned versionMajor = 1;
-unsigned versionMinor = 5;
+unsigned versionMinor = 6;
 
 // When the emulated CPU starts we execute the first million odd cycles in non-real-time (ie as fast as possible so the emulated 1541 becomes responsive to CBM-Browser asap)
 // During these cycles the CPU is executing the ROM self test routines (these do not need to be cycle accurate)
