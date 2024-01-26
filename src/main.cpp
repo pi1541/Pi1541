df2553e82e0bd9ad0ed36449e94410987c6e11d7
pottendo <pottendo@gmx.net>
2024-01-26 18:59:41 +0100

    version postfix

--- src/main.cpp
+++ src/main.cpp
@@ -58,7 +58,11 @@ extern "C"
 
 unsigned versionMajor = 1;
 unsigned versionMinor = 24;
-
+#if defined (__CIRCLE__)
+#define CV "c"
+#else
+#define CV
+#endif
 // When the emulated CPU starts we execute the first million odd cycles in non-real-time (ie as fast as possible so the emulated 1541 becomes responsive to CBM-Browser asap)
 // During these cycles the CPU is executing the ROM self test routines (these do not need to be cycle accurate)
 // ***1581*** Skip to AFCA (how many cycles is this?)
